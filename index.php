30b6d4cb006470b92182886943a9f54bacf15d56
vunam03 <164014080+vunam03@users.noreply.github.com>
2024-05-06 09:23:10 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,27 +1,29 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post">
+    tendangnhap <input type="text" name="user" size="15">
+   <br> matkhau <input type="password" name="pass" size="15">
+   <br> <input type="submit" name="submit" value="login" >
+   </form>
+    <?php
+        $tendangnhap= $_POST['user'];
+        $matkhau= $_POST['pass'];
 
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
+        if($tendangnhap == "admin" && $matkhau="123456"){
+            echo "<font color=red> Welcome to, ",$tendangnhap," <font>";
+        }
+        else{
+            echo"<font coler=red>tendangnhap hoac matkhau khong chinh xac, vui long dang nhap lai<font>";
+        }
+    ?>
 
-function sayHello($name) {
-	echo "Hello $name!";
-}
 
-?>
-
-<html>
-	<head>
-		<title>Visual Studio Code Remote :: PHP</title>
-	</head>
-	<body>
-		<?php 
-		
-		sayHello('remote world');
-			
-		phpinfo(); 
-			
-		?>
-	</body>
+</body>
 </html>
+        
